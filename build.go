e0458fe93698133fde9306910578e786b6e9ce0d
dan <dan@ezoic.com>
2015-09-02 17:07:14 -0700

    circle test

--- build.go
+++ build.go
@@ -1,1 +1,14 @@
 package circletest
+
+func StrInSlice(a string, s []string) bool {
+
+	isInSlice := false
+	for _, v := range s {
+		if v == a {
+			isInSlice = true
+			break
+		}
+	}
+	return isInSlice
+
+}
--- build_test.go
+++ build_test.go
@@ -3,8 +3,6 @@ package circletest
 import (
 	"runtime"
 	"testing"
-
-	"github.com/ezoic/sol/ezutil"
 )
 
 func Test_GolangVersion(t *testing.T) {
@@ -12,7 +10,7 @@ func Test_GolangVersion(t *testing.T) {
 	//to specify the version of golang.  therefore, make a test that fails when the version
 	//does not match the expected version
 	expectedVersions := []string{"go1.5"}
-	if ezutil.StrInSlice(runtime.Version(), expectedVersions) == false {
+	if StrInSlice(runtime.Version(), expectedVersions) == false {
 		t.Fatalf("Expected golang version: %+v, actual: %s\n", expectedVersions, runtime.Version())
 	}
 }
