c91eb6d9106f33f2640b1fc7f8f10eafa5259e22
dan <dan@ezoic.com>
2016-02-11 16:11:34 -0800

    force a test error and see what happens with go2xunit

--- build_test.go
+++ build_test.go
@@ -1,16 +1,15 @@
 package circletest
 
-import (
-	"runtime"
-	"testing"
-)
+import "testing"
 
 func Test_GolangVersion(t *testing.T) {
 	//when golang changes versions, it sometimes breaks code.  circleci does not allow
 	//to specify the version of golang.  therefore, make a test that fails when the version
 	//does not match the expected version
-	expectedVersions := []string{"go1.5"}
-	if StrInSlice(runtime.Version(), expectedVersions) == false {
-		t.Fatalf("Expected golang version: %+v, actual: %s\n", expectedVersions, runtime.Version())
-	}
+	//expectedVersions := []string{"go1.5"}
+	//if StrInSlice(runtime.Version(), expectedVersions) == false {
+	//	t.Fatalf("Expected golang version: %+v, actual: %s\n", expectedVersions, runtime.Version())
+	//}
+
+	t.Errorf("force a test failure")
 }
